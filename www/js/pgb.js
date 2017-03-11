3386a756b00de20837d012769fa2ef973e52dd81
Marcin Goryl <marcin.goryl@microsoft.wsei.edu.pl>
2017-03-11 16:58:57 +0100

    sds

---
+++ www/js/pgb.js
@@ -0,0 +1,36 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+function mcgorisInfo() {
+	
+	info = 'Siema, tu mcgoris ' + '\n' +
+		   'Imie : ' + 'Marcin' +  '\n' +
+		   'Nazwisko : ' + 'Goryl' +  '\n' +
+		   'E-mail : ' + 'mcgoris@o2.pl' +  '\n' +
+		   'Website : ' + 'mcastronomia.blogspot.com' + '\n' +
+		   'Grupa : ' + 'Informatyka i Ekonometria' + '\n' +
+		   'Studia : ' + '3 rok' + '\n' +;
+		   
+	navigator.notification.alert(info);
+	
+}
+	
+}
